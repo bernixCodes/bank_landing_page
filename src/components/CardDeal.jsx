34c4d9e2691f0135ef664772877408b3e9b53072
bernice <bernice@hubtel.com>
2022-12-11 09:43:02 +0000

    card details

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,5 +1,27 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br sm:block hidden /> in few easy steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore modi
+          placeat, nesciunt voluptatem aliquam quasi eos quod obcaecati soluta
+          iste.
+        </p>
+        <Button styles={"mt-10"} />
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
